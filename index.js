7b5b40e7fc5098ae3abd6156e704269bdcdc6387
suhel6 <161573939+suhel6@users.noreply.github.com>
2024-03-14 20:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ document.addEventListener("keydown", function (event) {
 
 function ring(k) {
     if (k == 'w') {
-        gana = new Audio('./sounds/crash.mp3');
+        gana = new Audio('./crash.mp3');
     }
     else {
         if (k == 'a') {
-            gana = new Audio('./sounds/kick-bass.mp3');
+            gana = new Audio('./kick-bass.mp3');
         }
         else {
             if (k == 's') {
-                gana = new Audio('./sounds/snare.mp3');
+                gana = new Audio('./snare.mp3');
             }
             else {
                 if (k == 'd') {
-                    gana = new Audio('./sounds/tom-1.mp3');
+                    gana = new Audio('./tom-1.mp3');
                 }
                 else {
                     if (k == 'j') {
-                        gana = new Audio('./sounds/tom-2.mp3');
+                        gana = new Audio('./tom-2.mp3');
                     }
                     else {
                         if (k == 'k') {
-                            gana = new Audio('./sounds/tom-3.mp3');
+                            gana = new Audio('./tom-3.mp3');
                         }
                         else {
                             if (k == 'l') {
-                                gana = new Audio('./sounds/tom-4.mp3');
+                                gana = new Audio('./tom-4.mp3');
                             }
                         }
                     }
